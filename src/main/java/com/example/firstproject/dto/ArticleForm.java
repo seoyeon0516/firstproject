721904669ec6a8c7e269a16eb9b002616a6ba62a
seoyeon0516 <0516psy@naver.com>
2026-08-17 18:06:06 +0900

    to 3.3 chapter

---
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -0,0 +1,26 @@
+package com.example.firstproject.dto;
+
+import com.example.firstproject.entity.Article;
+
+// DTO - 폼 데이터를 받아올 그릇
+public class ArticleForm {
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    public Article toEntity() {
+        return new Article(null, title, content);
+    }
+}
